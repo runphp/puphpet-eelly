38edafe7c3efca521dcd48a6e6e6975e1f664d14
runphp <runphp@qq.com>
2017-01-13 18:17:09 +0800

    add php-ext-memcached

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # puphpet-eelly
 Vagrant development environment for eelly phper, built using PuPHPet
 
+## info
+
+* centos7
+* php5.5
+* php-ext-memcached2.2.0
+* php-ext-mongodb1.2.2
+* php-ext-mongo1.6.14
+* php-ext-phalcon2.0.13
+* php-ext-redis3.1.0
+
 ## command
 ```
 # 启动
---
+++ puphpet/files/exec-once/php-ext-memcached.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# libmemcached
+cp /vagrant/install/php/libmemcached-1.0.18.tar.gz ./
+tar xzf libmemcached-1.0.18.tar.gz
+cd libmemcached-1.0.18
+./configure
+make
+make install
+
+# ext-memcached
+
+cd ..
+cp /vagrant/install/php/memcached-2.2.0.tgz ./
+tar xzf memcached-2.2.0.tgz
+cd memcached-2.2.0
+php-config
+./configure --disable-memcached-sasl
+make
+make install
+service php-fpm restart
